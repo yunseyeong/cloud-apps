7c8068b5e78f4871fa2200c57588a721ec371f6a
seyeong yun <seyeong.yun@medit.com>
2024-12-02 15:26:10 +0900

    build: MC-2 configure MFA with nx

--- apps/mfe2/src/app/app.spec.tsx
+++ apps/mfe2/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome cloud-apps/gi)).toBeTruthy();
+    expect(getByText(/Welcome mfe2/gi)).toBeTruthy();
   });
 });
---
+++ apps/mfe2/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="mfe2" />
+    </div>
+  );
+}
+
+export default App;
